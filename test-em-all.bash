07edeef7d9214dd86cf0f5439501db6dc912cb16
Mohamed Abdullahi <Mohamedsahal612@gmail.com>
2022-10-27 14:27:03 -0500

    fixed a bug in a bash file

--- test-em-all.bash
+++ test-em-all.bash
@@ -38,7 +38,7 @@ function assertCurl() {
 function assertEqual() {
 
   local expected=$1
-  local actual=$2
+  local actual=$1
 
   if [ "$actual" = "$expected" ]
   then
@@ -85,6 +85,7 @@ echo "Start Tests:" `date`
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
+# shellcheck disable=SC2199
 if [[ $@ == *"start"* ]]
 then
   echo "Restarting the test environment..."
@@ -98,33 +99,33 @@ waitForService curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS
 
 # Verify that a normal request works, expect three recommendations and three reviews
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
-assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | jq .productId)
-assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | json_pp .productId)
+assertEqual 3 $(echo $RESPONSE | json_pp ".recommendations | length")
+assertEqual 3 $(echo $RESPONSE | json_pp ".reviews | length")
 
 # Verify that a 404 (Not Found) error is returned for a non-existing productId ($PROD_ID_NOT_FOUND)
 assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
-assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
+assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | json_pp -r .message)"
 
 # Verify that no recommendations are returned for productId $PROD_ID_NO_RECS
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_RECS -s"
-assertEqual $PROD_ID_NO_RECS $(echo $RESPONSE | jq .productId)
-assertEqual 0 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual $PROD_ID_NO_RECS $(echo $RESPONSE | json_pp .productId)
+assertEqual 0 $(echo $RESPONSE | json_pp ".recommendations | length")
+assertEqual 3 $(echo $RESPONSE | json_pp ".reviews | length")
 
 # Verify that no reviews are returned for productId $PROD_ID_NO_REVS
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s"
-assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId)
-assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
-assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
+assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | json_pp .productId)
+assertEqual 3 $(echo $RESPONSE | json_pp ".recommendations | length")
+assertEqual 0 $(echo $RESPONSE | json_pp ".reviews | length")
 
 # Verify that a 422 (Unprocessable Entity) error is returned for a productId that is out of range (-1)
 assertCurl 422 "curl http://$HOST:$PORT/product-composite/-1 -s"
-assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
+assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | json_pp .message)"
 
 # Verify that a 400 (Bad Request) error error is returned for a productId that is not a number, i.e. invalid format
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
-assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | json_pp .message)"
 
 if [[ $@ == *"stop"* ]]
 then
